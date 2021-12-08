21dbb80bc8e2fa88dbe044c64dec6641f73d3bc6
Salim Mvoungui <salim.mvoungui@gmail.com>
2021-12-08 15:00:56 +0100

    Js påbörjat uppdate text

--- admin/js/update-post.js
+++ admin/js/update-post.js
@@ -1,39 +1,48 @@
-async function getPost() {
+window.onload = function () {
+  let queryString = location.search;
+
+  let postId = new URLSearchParams(window.location.search).get("id");
+
+  getPost(postId);
+  submitUpdate(postId);
+  console.log(postId);
+};
+
+async function getPost(postId) {
   try {
-    let response = await fetch("http://localhost:5000/posts");
+    let response = await fetch(`http://localhost:5000/posts/${postId}`);
     let post = await response.json();
-    console.log(post);
-    let areaValue = post[1].content;
-    console.log(areaValue);
+
+    let areaValue = post.content;
+
     document.getElementById("update-content").value = areaValue;
   } catch (error) {
     console.log(error);
   }
 }
-getPost();
 
-function submitUpdate() {
+function submitUpdate(postId) {
   let form = document.getElementById("update-form");
   form.addEventListener("submit", async function (e) {
     e.preventDefault();
     let formData = new FormData(form);
+
     formDataObject = {
       content: formData.get("content"),
     };
 
     try {
-      await fetch("http://localhost:5000/posts", {
+      await fetch(`http://localhost:5000/posts/${postId}`, {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(formDataObject),
       });
-
       console.log("hej");
+      location.replace("index.html");
     } catch (error) {
       console.log(error);
     }
   });
 }
-submitUpdate();
