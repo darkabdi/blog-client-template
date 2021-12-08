aa8b253beca0a0794bf5b81fc542ef41940adadc
abdi mohamed <aabdymohamed@gmail.com>
2021-12-08 13:41:35 +0100

    fetch för managepost e klar

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -3,18 +3,11 @@ window.onload = function() {
 }
 // let url = '/localhost:5000/posts'
 async function getPosts(){
-<<<<<<< HEAD
     let response= await (await fetch('http://localhost:5000/posts')).json()
-    console.log(response.content)
-    
     let content = document.getElementById("content")
     for (let post of response){
-      let blogs =  `<li>${response.content}</li>`
+      let blogs =  `<li>${post.content}</li>`
       content.innerHTML +=blogs
     }    
-=======
-    let response= await fetch('http://localhost:5000/posts')
-    
->>>>>>> 0c0fe173a1e19ca2cb2856ce6e004e8d3676d06d
     
 }
