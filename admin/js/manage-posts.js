fad01ad2be6aea9e1ebb26cbe162424f639950d6
abdi mohamed <aabdymohamed@gmail.com>
2021-12-08 13:54:03 +0100

    klar med data-id

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -4,10 +4,15 @@ window.onload = function() {
 // let url = '/localhost:5000/posts'
 async function getPosts(){
     let response= await (await fetch('http://localhost:5000/posts')).json()
-    let content = document.getElementById("content")
-    for (let post of response){
-      let blogs =  `<li>${post.content}</li>`
-      content.innerHTML +=blogs
-    }    
     
+    let posts =""
+    for (let post of response){
+     posts +=`
+     <li class="list-group-item">
+         <p>${post.content}</p>
+         <button id="update" data-id=${post._id}><a href="/Users/BIgABE/Desktop/YH 2021/BlogApiKlient/blog-client-template/admin/update-post.html?id=${post._id}">update</a></button>
+     </li>
+ `
+    }    
+    let content = document.getElementById("content").innerHTML+= posts
 }
