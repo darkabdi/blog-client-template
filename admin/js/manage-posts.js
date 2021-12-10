f535daf9e06c272c1b27ea2dec76f29fc6dd95ec
abdi mohamed <aabdymohamed@gmail.com>
2021-12-10 13:38:24 +0100

    fixat så det finns datum

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -7,12 +7,15 @@ async function getPosts(){
     
     let posts =""
     for (let post of response){
+        let postdate = new Date(post.date)
+        let formatedDate = `${postdate.getFullYear()}-${postdate.getMonth() + 1}-${postdate.getDate()}`
      posts +=`
      <li class="list-group-item" style="list-style: none;" >
-         <p>${post.content}</p>
+         <p>${post.content}</p> <br> ${formatedDate}</span> </p>
          <button id="update" data-id=${post._id}><a href="../admin/update-post.html?id=${post._id}">update</a></button>
          <button class="delete" data-id=${post._id}>delete</a></button>
      </li>`
+     console.log(formatedDate)
     }
 
     let deletebtn = document.getElementsByClassName("delete")
