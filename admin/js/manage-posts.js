533687b69bd33f086620097d1bf373cf85a96cd1
abdi mohamed <aabdymohamed@gmail.com>
2021-12-10 13:18:09 +0100

    ok

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -8,11 +8,20 @@ async function getPosts(){
     let posts =""
     for (let post of response){
      posts +=`
-     <li class="list-group-item">
+     <li class="list-group-item" style="list-style: none;" >
          <p>${post.content}</p>
          <button id="update" data-id=${post._id}><a href="../admin/update-post.html?id=${post._id}">update</a></button>
-     </li>
- `
-    }    
+         <button class="delete" data-id=${post._id}>delete</a></button>
+     </li>`
+    }
+
+    let deletebtn = document.getElementsByClassName("delete")
+    for (let links of deletebtn){
+        links.addEventListener("click",async function(e){
+            e.preventDefault()
+            console.log(e.target)
+        })
+    }
     let content = document.getElementById("content").innerHTML+= posts
+   
 }
