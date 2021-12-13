abfe6d4d667e394ddf38baaf8806171ba335958b
Juan Sagrera <j.sagrera77@gmail.com>
2021-12-13 21:01:43 +0100

    Klar med html och js på create-sidan

--- admin/js/create-post.js
+++ admin/js/create-post.js
@@ -2,21 +2,17 @@ CreateNewPost();
 
 function CreateNewPost() {
   let form = document.getElementById("create-form");
-  document.getElementById("create-content").value;
-  document.getElementById("author-input").value;
-  document.getElementById("title-input").value;
 
   form.addEventListener("submit", async function (e) {
     e.preventDefault();
 
     let formData = new FormData(form);
-    let formDataObject = {
+    let blogPost = {
       content: formData.get("create-text"),
       author: formData.get("author"),
       title: formData.get("title"),
+      tags: formData.getAll("tags"),
     };
-
-   
 
     try {
       await fetch("http://localhost:5000/posts", {
@@ -24,7 +20,7 @@ function CreateNewPost() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formDataObject),
+        body: JSON.stringify(blogPost),
       });
       location.replace("index.html");
     } catch (error) {
@@ -33,25 +29,10 @@ function CreateNewPost() {
   });
 }
 
-CreateNewPost();
+// function addTag () {
 
-// let serializeForm = function (form) {
-//     var obj = {};
-//     var formData = new FormData(form);
-//     // console.log(formData.getAll());
+// let select = document.getElementById('tags');
+// let value = select.options[select.selectedIndex].value;
+// console.log(value);
 
-//     for (var key of formData.keys()) {
-//         let inputData = formData.getAll(key);
-
-//         if (inputData.length > 1) {
-//             obj[key] = inputData;
-//         } else {
-//             obj[key] = inputData[0];
-//         }
-//     }
-
-//     // console.log(obj);
-//     return obj;
-// };
-
-// formDataObject = serializeForm(form);
+// }
