17632a6ba68e5f53b39c4380f8150dee7a2d0e7a
abdi mohamed <aabdymohamed@gmail.com>
2021-12-15 16:14:14 +0100

    kopplade ihop dokumenterna till varandra

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -1,7 +1,6 @@
 window.onload = function () {
   getPosts();
 };
-// let url = '/localhost:5000/posts'
 async function getPosts(){
     try {
         let response= await (await fetch('http://localhost:5000/posts')).json()
@@ -13,11 +12,13 @@ async function getPosts(){
      posts +=`
      <li class="list-group-item" style="list-style: none;" >
          <p>${post.content}</p> <br> ${formatedDate}</span> </p>
+         
          <button id="update" data-id=${post._id}><a href="../admin/update-post.html?id=${post._id}">update</a></button>
          <button class="delete" data-id=${post._id}>delete</a></button>
      </li>`
     }
     let content = document.getElementById("content").innerHTML+= posts
+    console.log(post.content)
    
     } catch (error) {
         
